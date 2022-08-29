0004fce2038c88c221a77e0b057d4ee07b6fc93f
Stuart Myers <stuart.myers@gmail.com>
2022-08-29 18:21:36 +0800

    Should avoid hard coding the game ID but yeah

--- src/app/gameStateHandler.tsx
+++ src/app/gameStateHandler.tsx
@@ -9,7 +9,7 @@ const intervalLength = 2000;
 
 export function GameStateHandler() {
   const params = {
-    gameID: 665,
+    gameID: 663,
     playerID: 3,
     authKey: '28df413b665604299807c461a7f3cae71c4176cb2b96afad04b84cf96d016258'
   };
