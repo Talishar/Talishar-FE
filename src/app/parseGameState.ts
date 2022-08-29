253d6b71237b246df152c3cb03ee7b66db49d21f
Stuart Myers <stuart.myers@gmail.com>
2022-08-29 18:16:05 +0800

    Thunkin all over the world

--- src/app/parseGameState.ts
+++ src/app/parseGameState.ts
@@ -4,9 +4,9 @@ export function ParseGameState(input: string) {
   const stringArray: string[] = input.toUpperCase().split('<BR>');
   // console.log(stringArray);
   const result: GameState = {
-    gameID: 664,
-    playerID: 3,
-    authKey: '28df413b665604299807c461a7f3cae71c4176cb2b96afad04b84cf96d016258',
+    gameID: 0,
+    playerID: 0,
+    authKey: '',
     playerOne: parseEQArray(stringArray[3]),
     playerTwo: {
       // AI or opposing player
