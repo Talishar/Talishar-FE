cc19aa54f0259c65fc01fcd28743b7fd762e2bd8
Stuart Myers <stuart.myers@gmail.com>
2022-08-30 23:47:47 +0800

    finish flexbox

--- src/App.tsx
+++ src/App.tsx
@@ -6,38 +6,13 @@ import { LeftColumn } from './game/leftColumn';
 import { RightColumn } from './game/rightColumn';
 import { HandZone } from './game/zones/handZone';
 
-const maxWidth = 1370;
-const maxHeight = 970;
-
 function App() {
-  const [dimension, setDimension] = React.useState(1.0);
-
-  React.useEffect(() => {
-    function handleResize() {
-      let ratio = 0;
-      if (
-        window.innerHeight / maxHeight <
-        (window.innerWidth - 75) / maxWidth
-      ) {
-        ratio = window.innerHeight / maxHeight;
-      } else {
-        ratio = (window.innerWidth - 75) / maxWidth;
-      }
-      setDimension(ratio);
-    }
-    window.addEventListener('resize', handleResize);
-    handleResize();
-  }, []);
-
   return (
     <div className="centering">
       <GameStateHandler />
       <div className="app">
         <LeftColumn />
-        <div
-          className="gameZone"
-          // style={{ transform: 'scale(' + dimension + ')' }}
-        >
+        <div className="gameZone">
           <HandZone isPlayer={false} />
           <Board />
           <HandZone isPlayer={true} />
