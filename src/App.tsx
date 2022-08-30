23db4c8d89d61f3a8a2b9e0a0e2280ca8e092642
Stuart Myers <stuart.myers@gmail.com>
2022-08-31 00:22:17 +0800

    Fix aspect ratio

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Board } from './game/board';
 import './App.css';
 import { GameStateHandler } from './app/gameStateHandler';
@@ -7,10 +7,24 @@ import { RightColumn } from './game/rightColumn';
 import { HandZone } from './game/zones/handZone';
 
 function App() {
+  const [maxWidth, setMaxWidth] = useState(1920);
+
+  useEffect(() => {
+    const calculateRatio = () => {
+      if (window.innerHeight < window.innerWidth) {
+        setMaxWidth((window.innerHeight * 16) / 9);
+      } else {
+        setMaxWidth(window.innerWidth);
+      }
+    };
+    window.addEventListener('resize', calculateRatio);
+    calculateRatio();
+  }, []);
+
   return (
     <div className="centering">
       <GameStateHandler />
-      <div className="app">
+      <div className="app" style={{ maxWidth: maxWidth }}>
         <LeftColumn />
         <div className="gameZone">
           <HandZone isPlayer={false} />
