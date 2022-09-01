836c0724d732e0b06d47ed2362b2f394ed07e123
Stuart Myers <stuart.myers@gmail.com>
2022-09-01 13:17:54 +0800

    Start combat chain

--- src/game/combatchain.tsx
+++ src/game/combatchain.tsx
@@ -1,6 +1,20 @@
 import React from 'react';
 import styles from './board.module.css';
+import { ChainLinks } from './elements/chainLinks';
+import { CurrentAttack } from './elements/currentAttack';
+import { Reactions } from './elements/reactions';
+import { TurnWidget } from './elements/turnWidget';
 
 export function CombatChain() {
-  return <div className={styles.combatChain}>COMBAT CHANE PEW PEW PEW</div>;
+  return (
+    <div className={styles.combatChain}>
+      <CurrentAttack />
+      COMBAT CHANE PEW PEW PEW
+      <div className={styles.chainCentre}>
+        <ChainLinks />
+        <Reactions />
+      </div>
+      <TurnWidget />
+    </div>
+  );
 }
