50101de84c24ca9d4530430936e66e12b335dd65
Stuart Myers <stuart.myers@gmail.com>
2022-09-08 17:34:56 +0800

    Scaling stuff

--- src/game/elements/TurnWidget.tsx
+++ src/game/elements/TurnWidget.tsx
@@ -5,10 +5,11 @@ import PassTurnDisplay from './PassTurnDisplay';
 import styles from './TurnWidget.module.css';
 
 const defaultInnerHeight = 940;
+const baseHeight = 210;
+const baseWidth = 350;
 
 export default function TurnWidget() {
   const [heightRatio, setHeightRatio] = useState(1);
-  console.log(window.innerHeight);
   useEffect(() => {
     function calculateWidgetHeight() {
       return window.innerHeight / defaultInnerHeight;
@@ -16,18 +17,24 @@ export default function TurnWidget() {
     setHeightRatio(calculateWidgetHeight());
   }, [window.innerHeight]);
   const style = { transform: `scale(${heightRatio})` };
-  console.log(style);
+  const containerStyle = {
+    height: `${baseHeight * heightRatio}px`,
+    width: `${baseWidth * heightRatio}px`
+  };
+  console.log(containerStyle);
   return (
-    <div className={styles.widgetContainer} style={style}>
-      <div className={styles.widgetGraphic}>
-        <div className={styles.widgetLeftCol}>
-          <ActionPointDisplay isPlayer={false} />
-          <PassTurnDisplay />
-          <ActionPointDisplay isPlayer />
-        </div>
-        <div className={styles.widgetRightCol}>
-          <HealthDisplay isPlayer={false} />
-          <HealthDisplay isPlayer />
+    <div className={styles.widgetContainer} style={containerStyle}>
+      <div className={styles.widgetScaler} style={style}>
+        <div className={styles.widgetGraphic}>
+          <div className={styles.widgetLeftCol}>
+            <ActionPointDisplay isPlayer={false} />
+            <PassTurnDisplay />
+            <ActionPointDisplay isPlayer />
+          </div>
+          <div className={styles.widgetRightCol}>
+            <HealthDisplay isPlayer={false} />
+            <HealthDisplay isPlayer />
+          </div>
         </div>
       </div>
     </div>
