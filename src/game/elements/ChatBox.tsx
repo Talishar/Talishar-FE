2806c42cd29a8b42ce39fa712684361ad7baaeee
Stuart Myers <stuart.myers@gmail.com>
2022-09-09 16:16:10 +0800

    smooth out chat box

--- src/game/elements/ChatBox.tsx
+++ src/game/elements/ChatBox.tsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useRef } from 'react';
+import { setSyntheticTrailingComments } from 'typescript';
 import { useAppSelector } from '../../app/Hooks';
 import { RootState } from '../../app/Store';
 import styles from './ChatBox.module.css';
@@ -8,7 +9,10 @@ export default function ChatBox() {
   const messagesEndRef = useRef<HTMLDivElement>(null);
 
   const scrollToBottom = () => {
-    messagesEndRef.current?.scrollIntoView({ behavior: 'smooth' });
+    messagesEndRef.current?.scrollIntoView({
+      behavior: 'smooth',
+      block: 'end'
+    });
   };
 
   useEffect(() => {
@@ -18,13 +22,19 @@ export default function ChatBox() {
   // TODO We really should not be dangerouslySetInnerHTML it's pretty bad mmkay
   return (
     <div className={styles.chatBoxContainer}>
-      <div className={styles.chatBox} ref={messagesEndRef}>
-        {chatLog &&
-          chatLog.map((chat, ix) => {
-            return (
-              <div dangerouslySetInnerHTML={{ __html: chat }} key={ix}></div>
-            );
-          })}
+      <div className={styles.chatBoxInner}>
+        <div className={styles.chatBox}>
+          {chatLog &&
+            chatLog.map((chat, ix) => {
+              return (
+                <div
+                  dangerouslySetInnerHTML={{ __html: chat }}
+                  key={ix}
+                  ref={messagesEndRef}
+                ></div>
+              );
+            })}
+        </div>
       </div>
       <form className={styles.chatInput}>Input</form>
     </div>
