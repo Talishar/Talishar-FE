3cd9f6548533a68fbc0bf3ffe24f29d4e2dcd387
Stuart Myers <stuart.myers@gmail.com>
2022-11-05 16:06:17 +0800

    typo

--- src/game/elements/currentAttack/CurrentAttack.tsx
+++ src/game/elements/currentAttack/CurrentAttack.tsx
@@ -37,7 +37,7 @@ export default function CurrentAttack() {
           <img
             className={styles.chainSymbols}
             src={defSymbol}
-            alt="attack symbol"
+            alt="defence symbol"
           />
         </div>
         <div className={styles.defDiv}>{defValue}</div>
