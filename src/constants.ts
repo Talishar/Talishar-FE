2f8a86a15eced6f15964cdfe2b7612553bb7a47d
Stuart Myers <stuart.myers@gmail.com>
2022-11-06 21:01:35 +0800

    constants for beta

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,6 @@
-export const API_URL = `http://localhost:41062/FaBOnline/`;
+// export const API_URL = `http://localhost:41062/FaBOnline/`;
 // export const API_URL = `https://talishar.net/game/`;
 // export const API_URL = `http://localhost/FaBOnline/`;
-// export const API_URL = `https://beta.talishar.net/game/`;
+export const API_URL = `https://beta.talishar.net/game/`;
 
 export const DEFAULT_PLAYMAT = `Default`;
