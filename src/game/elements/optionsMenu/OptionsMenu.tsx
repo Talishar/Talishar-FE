67bde506e811aa59c9dfbcfd7aa12d258f1c6759
Stuart Myers <stuart.myers@gmail.com>
2022-11-29 12:51:41 +0800

    Build out in game options

--- src/game/elements/optionsMenu/OptionsMenu.tsx
+++ src/game/elements/optionsMenu/OptionsMenu.tsx
@@ -1,12 +1,30 @@
+import { Field, Form, Formik } from 'formik';
 import React from 'react';
-import { useAppSelector } from '../../../app/Hooks';
+import { useAppDispatch, useAppSelector } from '../../../app/Hooks';
 import { RootState } from '../../../app/Store';
+import { closeOptionsMenu } from '../../../features/game/GameSlice';
 import styles from './OptionsMenu.module.css';
 
 export default function OptionsOverlay() {
   const optionsMenu = useAppSelector(
     (state: RootState) => state.game.optionsMenu
   );
+
+  const dispatch = useAppDispatch();
+
+  const initialValues = {
+    holdPriority: 'autoPass',
+    skipAttackReactions: false,
+    skipDefenseReactions: false,
+    manualTargeting: false,
+    attackSkip: 'neverSkip',
+    manualMode: false,
+    accessibilityMode: false,
+    mute: false,
+    disableChat: false,
+    disableStats: false,
+    casterMode: false
+  };
 
   if (
     optionsMenu === undefined ||
@@ -17,14 +35,18 @@ export default function OptionsOverlay() {
   }
 
   const closeOptions = () => {
-    console.log('closing options');
+    dispatch(closeOptionsMenu());
+  };
+
+  const submitOptions = () => {
+    console.log('submitting options');
   };
 
   return (
     <div className={styles.optionsContainer}>
       <div className={styles.optionsTitleContainer}>
         <div className={styles.optionsTitle}>
-          <h3 className={styles.title}>Options</h3>
+          <h2 className={styles.title}>Main Options</h2>
           (priority settings can be adjusted here)
         </div>
         <div className={styles.optionsMenuCloseIcon} onClick={closeOptions}>
@@ -34,6 +56,107 @@ export default function OptionsOverlay() {
             </h3>
           </div>
         </div>
+      </div>
+      <div className={styles.optionsContentContainer}>
+        <Formik
+          enableReinitialize={true}
+          initialValues={initialValues}
+          onSubmit={submitOptions}
+        >
+          {({ values }) => (
+            <Form>
+              <div>
+                <h3>Priority Settings:</h3>
+                <div className={styles.radioContainer}>
+                  <label className={styles.optionLabel}>
+                    <Field type="radio" name="holdPriority" value="autoPass" />
+                    Auto-Pass Priority
+                  </label>
+                  <label className={styles.optionLabel}>
+                    <Field
+                      type="radio"
+                      name="holdPriority"
+                      value="alwaysPass"
+                    />
+                    Always Pass Priority
+                  </label>
+                  <label className={styles.optionLabel}>
+                    <Field
+                      type="radio"
+                      name="holdPriority"
+                      value="alwaysHold"
+                    />
+                    Always Hold Priority
+                  </label>
+                  <label className={styles.optionLabel}>
+                    <Field
+                      type="radio"
+                      name="holdPriority"
+                      value="holdAllOpp"
+                    />
+                    Hold Priority for all Opponent Actions
+                  </label>
+                  <label className={styles.optionLabel}>
+                    <Field
+                      type="radio"
+                      name="holdPriority"
+                      value="holdOppAtt"
+                    />
+                    Hold Priority for all Opponent Attacks
+                  </label>
+                </div>
+                <h3>Skip overrides</h3>
+                <div className={styles.radioContainer}>
+                  <label className={styles.optionLabel}>
+                    <Field type="checkbox" name="skipAttackReactions" />
+                    Skip Attack Reactions
+                  </label>
+                  <label className={styles.optionLabel}>
+                    <Field type="checkbox" name="skipDefenseReactions" />
+                    Skip Defense Reactions
+                  </label>
+                  <label className={styles.optionLabel}>
+                    <Field type="checkbox" name="manualTargeting" />
+                    Manual Targeting
+                  </label>
+                </div>
+                <h3>Attack Shortcut Threshold</h3>
+                <div className={styles.radioContainer}>
+                  <label className={styles.optionLabel}>
+                    <Field type="radio" name="attackSkip" value="neverSkip" />
+                    Never Skip Attacks
+                  </label>
+                  <label className={styles.optionLabel}>
+                    <Field type="radio" name="attackSkip" value="skipOnes" />
+                    Skip 1 Power Attacks
+                  </label>
+                  <label className={styles.optionLabel}>
+                    <Field type="radio" name="attackSkip" value="skipAll" />
+                    Skip All Attacks
+                  </label>
+                </div>
+                <div className={styles.radioContainer}>
+                  <label className={styles.optionLabel}>
+                    <Field type="checkbox" name="manualMode" />
+                    Manual Mode
+                  </label>
+                  <label className={styles.optionLabel}>
+                    <Field type="checkbox" name="accessibilityMode" />
+                    Accessibility Mode
+                  </label>
+                  <label className={styles.optionLabel}>
+                    <Field type="checkbox" name="mute" />
+                    Mute
+                  </label>
+                  <label className={styles.optionLabel}>
+                    <Field type="checkbox" name="disableChat" />
+                    Manual Mode
+                  </label>
+                </div>
+              </div>
+            </Form>
+          )}
+        </Formik>
       </div>
     </div>
   );
