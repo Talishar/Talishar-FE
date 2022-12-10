ed5cf76b5bd2688c5e5339f8e7188691d8f69197
Stuart Myers <stuart.myers@gmail.com>
2022-12-10 17:38:30 +0800

    test descriptions

--- src/game/elements/gemSlider/GemSlider.test.tsx
+++ src/game/elements/gemSlider/GemSlider.test.tsx
@@ -12,19 +12,19 @@ it('is empty when gem is null', () => {
   expect(container.childElementCount).toEqual(0);
 });
 
-it('is empty when gem is 0', () => {
+it('is empty when gem is "none"', () => {
   const { container } = renderWithProviders(<GemSlider gem={'none'} />);
 
   expect(container.childElementCount).toEqual(0);
 });
 
-it('is inactive when gem is inactive', () => {
+it('is inactive when gem is "inactive"', () => {
   renderWithProviders(<GemSlider gem={'inactive'} />);
   const image = document.querySelector('img') as HTMLImageElement;
   expect(image.src).toContain('hexagonGrayGem.png');
 });
 
-it('is active when gem is active', () => {
+it('is active when gem is "active"', () => {
   renderWithProviders(<GemSlider gem={'active'} />);
   const image = document.querySelector('img') as HTMLImageElement;
   expect(image.src).toContain('hexagonRedGemGlow.png');
