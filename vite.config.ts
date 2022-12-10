f3fe5d3d460f7ab7ab9c8c6faa3968fbe46bbb2f
Stuart Myers <stuart.myers@gmail.com>
2022-12-10 18:49:26 +0800

    update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,6 @@ import svgrPlugin from 'vite-plugin-svgr';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: './',
   plugins: [react(), viteTsconfigPaths(), svgrPlugin()]
 });
