1b4ee5a444bb1e0a1ab289f3f50aa1a3ce166f3c
Stuart Myers <stuart.myers@gmail.com>
2022-12-12 13:16:41 +0800

    tidy

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,7 @@
 // For dev env use local proxy, on production connect directly to server.
+export const GAME_LIMIT_LIVE = 10000;
+export const GAME_LIMIT_BETA = 1000;
+
 export const API_URL_BETA = import.meta.env.DEV
   ? `/api/beta/`
   : 'https://beta.talishar.net/game';
@@ -9,11 +12,9 @@ export const API_URL_DEV = import.meta.env.DEV
   ? `/api/dev/`
   : 'https://talishar.net/game';
 
+// what playmat is the default
 export const DEFAULT_PLAYMAT = `Default`;
 
 // How long in ms for a click versus 'long press'
 // TODO: make this user definable
 export const LONG_PRESS_TIMER = 250;
-
-export const GAME_LIMIT_LIVE = 10000;
-export const GAME_LIMIT_BETA = 1000;
