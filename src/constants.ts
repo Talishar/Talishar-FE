08d4c076715704d2e4b2a2b97da705e218ce02f8
Stuart Myers <stuart.myers@gmail.com>
2022-12-12 12:59:31 +0800

    proxy to servers

--- src/constants.ts
+++ src/constants.ts
@@ -1,18 +1,7 @@
-// running in BE docker:
-// export const API_URL_BETA = `http://localhost:8080/FaBOnline/`;
-// export const API_URL_LIVE = `https://talishar.net/game/`;
-
-// running in old BE xampp docker:
-// export const API_URL_BETA = `http://localhost:41062/FaBOnline/`;
-// export const API_URL_LIVE = `https://talishar.net/game/`;
-
-// running on self-hoted (native) XAMPP:
-// export const API_URL_BETA = `http://localhost/FaBOnline/`;
-// export const API_URL_LIVE = `https://talishar.net/game/`;
-
 // running on beta/live on tinterwebs:
-export const API_URL_BETA = `https://beta.talishar.net/game/`;
-export const API_URL_LIVE = `https://talishar.net/game/`;
+export const API_URL_BETA = `/api/beta/`;
+export const API_URL_LIVE = `/api/prod/`;
+export const API_URL_DEV = `/api/dev`;
 
 export const DEFAULT_PLAYMAT = `Default`;
 
