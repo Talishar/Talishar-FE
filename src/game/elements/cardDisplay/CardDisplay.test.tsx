c1e7d2631cc9e0f947f690b6b7dd50b68bd666d3
Stuart Myers <stuart.myers@gmail.com>
2022-12-15 14:48:58 +0800

    update to test only for PR

--- src/game/elements/cardDisplay/CardDisplay.test.tsx
+++ src/game/elements/cardDisplay/CardDisplay.test.tsx
@@ -5,8 +5,3 @@ import { renderWithProviders } from '../../../utils/TestUtils';
 it('renders without crashing', () => {
   renderWithProviders(<CardDisplay />);
 });
-
-it('deliberate test to fail', () => {
-  renderWithProviders(<CardDisplay />);
-  expect(true).toEqual(false);
-});
