822ef38269414f591749ac1f4de4e26eb57a6c0a
Stuart Myers <stuart.myers@gmail.com>
2022-12-17 14:46:47 +0800

    some updated

--- src/game/elements/gemSlider/GemSlider.test.tsx
+++ src/game/elements/gemSlider/GemSlider.test.tsx
@@ -1,6 +1,3 @@
-/**
- * @vitest-environment jsdom
- */
 import React from 'react';
 import GemSlider from './GemSlider';
 import { renderWithProviders } from '../../../utils/TestUtils';
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,3 +1,8 @@
 /// <reference types="vite/client" />
 
 declare module '*.css';
+declare module '*.png';
+declare module '*.svg';
+declare module '*.jpeg';
+declare module '*.jpg';
+declare module '*.webp';
