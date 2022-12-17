fffef0f90b51e527d2ec0a375c928b30de6776bf
Stuart Myers <LaustinSpayce@users.noreply.github.com>
2022-12-17 17:55:06 +0000

    add a test

--- src/game/elements/countersOverlay/CountersOverlay.test.tsx
+++ src/game/elements/countersOverlay/CountersOverlay.test.tsx
@@ -5,3 +5,9 @@ import { renderWithProviders } from '../../../utils/TestUtils';
 it('renders without crashing', () => {
   renderWithProviders(<CountersOverlay cardNumber="WTR001" />);
 });
+
+it('dislpays a number', () => {
+  renderWithProviders(<CountersOverlay cardNumber="WTR001" num={3} />);
+  const div = document.querySelector('div');
+  expect(div).toContain('3');
+}); //might fail.
