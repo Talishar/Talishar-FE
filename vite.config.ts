c406c986aa590bd7d670fdc2a8c7f37d297fdba3
Stuart Myers <stuart.myers@gmail.com>
2022-12-17 13:15:34 +0800

    update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+/// <reference types="vitest" />
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import viteTsconfigPaths from 'vite-tsconfig-paths';
@@ -45,5 +46,6 @@ export default defineConfig({
         rewrite: (path) => path.replace(/api\/dev\//, '')
       }
     }
-  }
+  },
+  test: { globals: true }
 });
