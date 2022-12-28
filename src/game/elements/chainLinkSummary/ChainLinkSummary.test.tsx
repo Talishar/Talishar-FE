9989252929d44c75b917a3817c1f6342c9f0290f
Stuart Myers <stuart.myers@gmail.com>
2022-12-28 22:19:53 +0800

    fix a test huzzah

--- src/game/elements/chainLinkSummary/ChainLinkSummary.test.tsx
+++ src/game/elements/chainLinkSummary/ChainLinkSummary.test.tsx
@@ -1,15 +1,11 @@
 import React from 'react';
-import { Provider } from 'react-redux';
 import { store } from '../../../app/Store';
-import { createRoot } from 'react-dom/client';
 import ChainLinkSummaryContainer from './ChainLinkSummary';
 import { renderWithProviders } from '../../../utils/TestUtils';
-import { OfflineTestingGameState } from '../../../features/game/InitialGameState';
 import {
   setGameStart,
   showChainLinkSummary
 } from '../../../features/game/GameSlice';
-import { prettyDOM, waitForElementToBeRemoved } from '@testing-library/react';
 
 it('renders without crashing isPlayer true', () => {
   const div = renderWithProviders(<ChainLinkSummaryContainer />);
@@ -21,11 +17,8 @@ it('does something', async () => {
   store.dispatch(
     setGameStart({ playerID: 1, gameID: 123456, authKey: '12345' })
   );
-  console.log('store dispatched I guess');
   const div = renderWithProviders(<ChainLinkSummaryContainer />, {
     store: testStore
   });
-  console.log(div.debug());
-  await waitForElementToBeRemoved(div.queryByText('Loading...'));
-  console.log(div.debug());
+  await div.findByText('Zipper Hit');
 });
