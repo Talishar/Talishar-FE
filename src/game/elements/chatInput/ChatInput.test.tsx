12758c9362e11d360d394ff1878802bfbd3021db
Stuart Myers <stuart.myers@gmail.com>
2023-01-05 23:13:00 +0800

    fix test

--- src/game/elements/chatInput/ChatInput.test.tsx
+++ src/game/elements/chatInput/ChatInput.test.tsx
@@ -2,14 +2,14 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import { store } from '../../../app/Store';
 import { createRoot } from 'react-dom/client';
-import ChatBox from './ChatInput';
+import ChatInput from './ChatInput';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   const root = createRoot(div);
   root.render(
     <Provider store={store}>
-      <ChatBox />
+      <ChatInput />
     </Provider>
   );
 });
