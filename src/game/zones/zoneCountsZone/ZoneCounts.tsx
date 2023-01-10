4e0741482253ec9c3676e6a4b53b55f63751a729
Stuart Myers <LaustinSpayce@users.noreply.github.com>
2023-01-10 23:14:22 +0800

    refactor cardzones

--- src/game/zones/zoneCountsZone/ZoneCounts.tsx
+++ src/game/zones/zoneCountsZone/ZoneCounts.tsx
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
   GiAngelOutfit,
   GiCannon,
+  GiCircleClaws,
   GiFluffySwirl,
   GiHand,
   GiStack,
@@ -14,44 +15,122 @@ import Displayrow from '../../../interface/Displayrow';
 import styles from './ZoneCounts.module.css';
 
 export const ZoneCounts = (prop: Displayrow) => {
-  const { isPlayer } = prop;
-  const dispatch = useAppDispatch();
-
-  const soulCount = useAppSelector((state: RootState) =>
-    isPlayer ? state.game.playerOne.SoulCount : state.game.playerTwo.SoulCount
+  return (
+    <div className={styles.container}>
+      <div className={styles.column}>
+        <HandCount {...prop} />
+        <GraveyardCount {...prop} />
+        <BanishCount {...prop} />
+      </div>
+      <div className={styles.column}>
+        <DeckCount {...prop} />
+        <PitchCount {...prop} />
+        <ArsenalCount {...prop} />
+        <SoulCount {...prop} />
+      </div>
+    </div>
   );
+};
+
+const HandCount = (prop: Displayrow) => {
+  const { isPlayer } = prop;
   const handCount = useAppSelector((state: RootState) =>
     isPlayer
       ? state.game.playerOne.Hand?.length
       : state.game.playerTwo.Hand?.length
   );
+
+  return (
+    <div title="Hand" className={styles.item}>
+      <GiHand /> {handCount}
+    </div>
+  );
+};
+
+const GraveyardCount = (prop: Displayrow) => {
+  const { isPlayer } = prop;
   const graveyardCount = useAppSelector((state: RootState) => {
-    return (
-      (isPlayer
-        ? state.game.playerOne.GraveyardCount
-        : state.game.playerTwo.GraveyardCount) ?? 0
-    );
+    return isPlayer
+      ? state.game.playerOne.Graveyard?.length
+      : state.game.playerTwo.Graveyard?.length;
   });
 
+  return (
+    <div title="Graveyard" className={styles.item}>
+      <GiTombstone /> {graveyardCount}
+    </div>
+  );
+};
+
+const BanishCount = (prop: Displayrow) => {
+  const { isPlayer } = prop;
   const banishCount = useAppSelector((state: RootState) => {
+    return isPlayer
+      ? state.game.playerOne.Banish?.length
+      : state.game.playerTwo.Banish?.length;
+  });
+
+  return (
+    <div title="Banish" className={styles.item}>
+      <GiFluffySwirl /> {banishCount}
+    </div>
+  );
+};
+
+const DeckCount = (prop: Displayrow) => {
+  const { isPlayer } = prop;
+  const pitchCount = useAppSelector((state: RootState) => {
     return (
       (isPlayer
-        ? state.game.playerOne.BanishCount
-        : state.game.playerTwo.BanishCount) ?? 0
+        ? state.game.playerOne.DeckSize
+        : state.game.playerTwo.DeckSize) ?? 0
     );
   });
 
+  return (
+    <div title="Deck" className={styles.item}>
+      <GiStack /> {pitchCount}
+    </div>
+  );
+};
+
+const PitchCount = (prop: Displayrow) => {
+  const { isPlayer } = prop;
   const pitchCount = useAppSelector((state: RootState) => {
     return isPlayer
       ? state.game.playerOne.Pitch?.length
       : state.game.playerTwo.Pitch?.length;
   });
 
+  return (
+    <div title="Pitch" className={styles.item}>
+      <GiCircleClaws /> {pitchCount}
+    </div>
+  );
+};
+
+const ArsenalCount = (prop: Displayrow) => {
+  const { isPlayer } = prop;
   const arsenalCount = useAppSelector((state: RootState) => {
     return isPlayer
       ? state.game.playerOne.Arsenal?.length
       : state.game.playerTwo.Arsenal?.length;
   });
+
+  return (
+    <div title="Arsenal" className={styles.item}>
+      <GiCannon /> {arsenalCount}
+    </div>
+  );
+};
+
+const SoulCount = (prop: Displayrow) => {
+  const [hasSoul, setHasSoul] = useState(false);
+  const dispatch = useAppDispatch();
+  const { isPlayer } = prop;
+  const soulCount = useAppSelector((state: RootState) =>
+    isPlayer ? state.game.playerOne.SoulCount : state.game.playerTwo.SoulCount
+  );
 
   const soulDisplay = () => {
     const playerPronoun = isPlayer ? 'Your' : 'Your Opponents';
@@ -64,32 +143,23 @@ export const ZoneCounts = (prop: Displayrow) => {
     );
   };
 
+  if (!hasSoul && soulCount != undefined && soulCount > 0) {
+    setHasSoul(true);
+  }
+
   return (
-    <div className={styles.container}>
-      <div className={styles.column}>
-        <div title="Hand" className={styles.item}>
-          <GiHand /> {handCount}
+    <div>
+      {!!hasSoul ? (
+        <div
+          title="Soul"
+          className={styles.clickableItem}
+          onClick={soulDisplay}
+        >
+          <GiAngelOutfit /> {soulCount}
         </div>
-        <div title="Graveyard" className={styles.item}>
-          <GiTombstone /> {graveyardCount}
-        </div>
-        <div title="banish" className={styles.item}>
-          <GiFluffySwirl /> {banishCount}
-        </div>
-      </div>
-      <div className={styles.column}>
-        <div title="pitch" className={styles.item}>
-          <GiStack /> {pitchCount}
-        </div>
-        <div title="arsenal" className={styles.item}>
-          <GiCannon /> {arsenalCount}
-        </div>
-        {!!soulCount && (
-          <div title="soul" className={styles.item} onClick={soulDisplay}>
-            <GiAngelOutfit /> {soulCount}
-          </div>
-        )}
-      </div>
+      ) : (
+        <div className={styles.item}> </div>
+      )}
     </div>
   );
 };
