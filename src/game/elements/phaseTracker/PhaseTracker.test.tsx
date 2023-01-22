2fb7073745b5581ee957fce4368237c077f59ce6
Stuart Myers <LaustinSpayce@users.noreply.github.com>
2023-01-22 16:07:53 +0800

    update snapshot tests

--- src/game/elements/phaseTracker/PhaseTracker.test.tsx
+++ src/game/elements/phaseTracker/PhaseTracker.test.tsx
@@ -1,15 +1,9 @@
 import React from 'react';
-import { Provider } from 'react-redux';
-import { store } from '../../../app/Store';
-import { createRoot } from 'react-dom/client';
 import PhaseTracker from './PhaseTracker';
+import { renderWithProviders } from '../../../utils/TestUtils';
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  const root = createRoot(div);
-  root.render(
-    <Provider store={store}>
-      <PhaseTracker />
-    </Provider>
-  );
+  const div = renderWithProviders(<PhaseTracker />);
+  const display = document.querySelector('div');
+  expect(display).toMatchSnapshot();
 });
