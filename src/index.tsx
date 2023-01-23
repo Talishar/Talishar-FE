eb0e021b1acaaa0e20ef97f35a32e45c35c8670e
Stuart Myers <LaustinSpayce@users.noreply.github.com>
2023-01-23 11:06:35 +0800

    basic routes

--- src/index.tsx
+++ src/index.tsx
@@ -2,18 +2,26 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { store } from './app/Store';
-import App from './routes/game/play/play';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
 import './index.css';
+import Index from 'routes/index';
 
 // Because we *must* have a root else the site won't work at all.
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 const container = document.getElementById('root')!;
 const root = createRoot(container);
 
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Index />
+  }
+]);
+
 root.render(
   <Provider store={store}>
-    <App />
+    <RouterProvider router={router} />
   </Provider>
 );
 
---
+++ src/routes/user/User.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Index = () => {
+  return <div>HELLO USER</div>;
+};
+
+export default Index;
