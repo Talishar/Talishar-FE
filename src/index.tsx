f0333650319b66de78558a969c52e8322405870e
Stuart Myers <LaustinSpayce@users.noreply.github.com>
2023-01-23 15:38:14 +0800

    fix dodgy import

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,7 @@ import { Provider } from 'react-redux';
 import { store } from './app/Store';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
-import Index from 'routes/index';
+import Index from './routes/index/Index';
 import { ErrorPage } from 'errorPage';
 import Play from 'routes/game/play/Play';
 import './index.scss';
