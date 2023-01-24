631c7cf82c3dbbfd44e364914b90e1f970397555
Stuart Myers <LaustinSpayce@users.noreply.github.com>
2023-01-24 18:30:13 +0800

    Stacking permanents

--- src/routes/game/components/zones/permanentsZone/PermanentsZone.tsx
+++ src/routes/game/components/zones/permanentsZone/PermanentsZone.tsx
@@ -4,12 +4,26 @@ import { RootState } from 'app/Store';
 import Displayrow from 'interface/Displayrow';
 import CardDisplay from '../../elements/cardDisplay/CardDisplay';
 import styles from './PermanentsZone.module.css';
+import { Card } from 'features/Card';
+import isEqual from 'react-fast-compare';
+import { FaTimes } from 'react-icons/fa';
+import classNames from 'classnames';
+import { shallowEqual } from 'react-redux';
+
+export interface CardStack {
+  card: Card;
+  count: number;
+}
 
 export default function PermanentsZone(prop: Displayrow) {
   const { isPlayer } = prop;
 
-  const permanents = useAppSelector((state: RootState) =>
-    isPlayer ? state.game.playerOne.Permanents : state.game.playerTwo.Permanents
+  const permanents = useAppSelector(
+    (state: RootState) =>
+      isPlayer
+        ? state.game.playerOne.Permanents
+        : state.game.playerTwo.Permanents,
+    shallowEqual
   );
 
   if (permanents === undefined || permanents.length === 0) {
@@ -22,8 +36,35 @@ export default function PermanentsZone(prop: Displayrow) {
     );
   }
 
-  // TODO: Stack permanents that are the same.
-  // We probably need to use some deepEqual library to assist with this.
+  const sortedPermanents = [...permanents];
+  sortedPermanents.sort((a, b) => a.cardNumber.localeCompare(b.cardNumber));
+
+  // Stack cards.
+  let initialCardStack: CardStack[] = [];
+  const cardStackArray = sortedPermanents.reduce((accumulator, currentCard) => {
+    const cardCopy = { ...currentCard };
+    const storedADO = currentCard.actionDataOverride;
+    cardCopy.actionDataOverride = '';
+    let isInAccumulator = false;
+    let index = 0;
+    // is current card in the cardStackArray already?
+    for (const [ix, cardStack] of accumulator.entries()) {
+      cardCopy.actionDataOverride = cardStack.card.actionDataOverride;
+      if (isEqual(cardStack.card, cardCopy)) {
+        isInAccumulator = true;
+        index = ix;
+        break;
+      }
+    }
+    // if it is, +1 to count
+    if (isInAccumulator) {
+      accumulator[index].count = accumulator[index].count + 1;
+      return accumulator;
+    }
+    // if it is not, append to accumulator.
+    accumulator.push({ card: currentCard, count: 1 });
+    return accumulator;
+  }, initialCardStack);
 
   return (
     <div className={styles.permanentsWrapper}>
@@ -31,10 +72,26 @@ export default function PermanentsZone(prop: Displayrow) {
         <div>Permanents</div>
       </div>
       <div className={styles.permanentsZone}>
-        {permanents.map((card, ix) => {
+        {cardStackArray.map((cardStack, ix) => {
+          const cardContainerStyles = classNames(
+            {
+              [styles.stacked]: cardStack.count > 1
+            },
+            styles.cardContainer
+          );
           return (
-            <div key={ix.toString()} className={styles.cardContainer}>
-              <CardDisplay card={card} key={ix.toString()} />
+            <div>
+              <div key={ix.toString()} className={cardContainerStyles}>
+                <CardDisplay card={cardStack.card} key={ix.toString()} />
+                {cardStack.count > 1 && (
+                  <div
+                    title={`Stack of ${cardStack.count}`}
+                    className={styles.counter}
+                  >
+                    × {cardStack.count}
+                  </div>
+                )}
+              </div>
             </div>
           );
         })}
