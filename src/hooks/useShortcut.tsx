5532b5c91b28530b0e93a3c4e05fb40f1a049b77
Tetsuo Matsumura <tet.m@hotmail.com>
2023-01-25 05:07:25 -0300

    feat: :sparkles: add useShortcut hook

---
+++ src/hooks/useShortcut.tsx
@@ -0,0 +1,27 @@
+import { useCallback, useEffect, useLayoutEffect, useRef } from "react";
+
+const useShortcut = (keys: Array<String>, callback: Function) => {
+  // callback ref pattern
+  const callbackRef = useRef(callback);
+  useLayoutEffect(() => {
+    callbackRef.current = callback;
+  })
+
+  const handleKeyPress = useCallback(
+    (event: KeyboardEvent) => {
+      if (keys.some((key: String) => event.key === key)) {
+        callbackRef.current(event);
+      }
+    },
+    [keys]
+  );
+
+  useEffect(() => {
+    document.addEventListener("keydown", handleKeyPress);
+
+    return () =>
+      document.removeEventListener("keydown", handleKeyPress);
+  }, [handleKeyPress]);
+}
+
+export default useShortcut;
