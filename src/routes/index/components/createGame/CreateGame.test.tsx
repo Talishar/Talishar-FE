146399b85af2b524b250b9d662be8f965392be9f
Stuart Myers <LaustinSpayce@users.noreply.github.com>
2023-01-29 15:32:41 +0800

    fix failing test

--- src/routes/index/components/createGame/CreateGame.test.tsx
+++ src/routes/index/components/createGame/CreateGame.test.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { renderWithProviders } from 'utils/TestUtils';
-import CreateGameAPI from './CreateGame';
+import CreateGame from './CreateGame';
 
 it('renders without crashing', () => {
   renderWithProviders(<CreateGame />);
