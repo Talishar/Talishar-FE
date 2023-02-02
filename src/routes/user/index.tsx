965573f8c99b90bb31bd983ea9ecba459b982b77
MattMcCherry <matt.mccherry@gmail.com>
2023-02-02 12:26:56 +0000

    Add profile and decks pages

---
+++ src/routes/user/index.tsx
@@ -0,0 +1,3 @@
+export * from './login';
+export * from './decks';
+export * from './profile';
---
+++ src/routes/user/profile/index.tsx
@@ -0,0 +1,5 @@
+export const ProfilePage = () => {
+    return (
+        <div>Profile page</div>
+    );
+}
