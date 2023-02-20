47a5b7db92d027c4813a47063f93edbc455641ef
Stuart Myers <LaustinSpayce@users.noreply.github.com>
2023-02-20 22:22:09 +0800

    Start gridding

--- src/routes/game/components/board/Board.tsx
+++ src/routes/game/components/board/Board.tsx
@@ -7,21 +7,26 @@ import PlayerPrompt from '../elements/playerPrompt/PlayerPrompt';
 import ChainLinkSummaryContainer from '../elements/chainLinkSummary/ChainLinkSummary';
 import PlayerBoardGrid from '../playerBoardGrid/PlayerBoardGrid';
 import OpponentBoardGrid from '../opponentBoardGrid/OpponentBoardGrid';
+import GridBoard from './../gridBoard';
 
 export interface playAreaDimensions {
   dimension: number;
 }
 
 export function Board() {
-  return (
-    <div className={styles.gameBoard}>
-      <OpponentBoardGrid />
-      <CombatChain />
-      <PlayerPrompt />
-      <PlayerBoardGrid />
-      <CardListZone />
-      <ChainLinkSummaryContainer />
-      <ActiveLayersZone />
-    </div>
-  );
+  const optionA = false;
+  if (optionA) {
+    return (
+      <div className={styles.gameBoard}>
+        <OpponentBoardGrid />
+        <CombatChain />
+        <PlayerPrompt />
+        <PlayerBoardGrid />
+        <CardListZone />
+        <ChainLinkSummaryContainer />
+        <ActiveLayersZone />
+      </div>
+    );
+  }
+  return <GridBoard />;
 }
