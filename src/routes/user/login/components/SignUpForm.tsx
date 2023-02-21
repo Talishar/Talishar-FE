11a30f6da3eb976547a941022d2c7c90933d952c
Stuart Myers <LaustinSpayce@users.noreply.github.com>
2023-02-21 22:29:56 +0800

    improve sign up form

--- src/routes/user/login/components/SignUpForm.tsx
+++ src/routes/user/login/components/SignUpForm.tsx
@@ -1,13 +1,15 @@
 import { useSignUpMutation } from 'features/api/apiSlice';
-import { FormikProvider, useFormik } from 'formik';
+import { ErrorMessage, FormikProvider, getIn, useFormik } from 'formik';
 import styles from './LoginForm.module.css';
 import { Link } from 'react-router-dom';
 import classNames from 'classnames';
 import { signUpValidationSchema } from './validation';
 import { FaExclamationCircle } from 'react-icons/fa';
+import { useState } from 'react';
 
 export const SignUpForm = () => {
   const [signup, signupResult] = useSignUpMutation();
+  const [hasSubmitted, setHasSubmitted] = useState(false);
 
   const formik = useFormik({
     initialValues: {
@@ -18,6 +20,7 @@ export const SignUpForm = () => {
     },
     validationSchema: signUpValidationSchema,
     onSubmit: async (values) => {
+      setHasSubmitted(true);
       try {
         console.log('values', values);
       } catch (err) {
@@ -29,6 +32,7 @@ export const SignUpForm = () => {
   for (const [key, value] of Object.entries(formik.errors)) {
     errorArray.push(String(value));
   }
+
   return (
     <div>
       <h2>Sign Up</h2>
@@ -42,6 +46,7 @@ export const SignUpForm = () => {
               id="userID"
               placeholder="bravo"
               onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
               value={formik.values.userID}
               aria-invalid={
                 (formik.errors.userID && formik.touched.userID) as
@@ -49,7 +54,9 @@ export const SignUpForm = () => {
                   | undefined
               }
             />
-
+            <div className={styles.fieldError}>
+              <ErrorMessage name="userID" />
+            </div>
             <label htmlFor="email">Email</label>
             <input
               type="email"
@@ -57,6 +64,7 @@ export const SignUpForm = () => {
               id="email"
               placeholder="bravo@talishar.net"
               onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
               value={formik.values.email}
               aria-invalid={
                 (formik.errors.email && formik.touched.email) as
@@ -64,6 +72,9 @@ export const SignUpForm = () => {
                   | undefined
               }
             />
+            <div className={styles.fieldError}>
+              <ErrorMessage name="email" />
+            </div>
             <label htmlFor="password">Password</label>
             <input
               type="password"
@@ -71,6 +82,7 @@ export const SignUpForm = () => {
               id="password"
               placeholder="********"
               onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
               value={formik.values.password}
               aria-invalid={
                 (formik.errors.password && formik.touched.password) as
@@ -78,6 +90,9 @@ export const SignUpForm = () => {
                   | undefined
               }
             />
+            <div className={styles.fieldError}>
+              <ErrorMessage name="password" />
+            </div>
             <label htmlFor="passwordConfirm">Confirm Password</label>
             <input
               type="password"
@@ -85,21 +100,28 @@ export const SignUpForm = () => {
               id="passwordConfirm"
               placeholder="********"
               onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
               value={formik.values.passwordConfirm}
               aria-invalid={
                 (formik.errors.passwordConfirm &&
                   formik.touched.passwordConfirm) as boolean | undefined
               }
             />
+            <div className={styles.fieldError}>
+              <ErrorMessage name="passwordConfirm" />
+            </div>
             <button
-              type="submit"
               disabled={formik.isSubmitting}
               aria-busy={formik.isSubmitting}
               className={styles.submitButton}
+              onClick={() => {
+                setHasSubmitted(true);
+                formik.handleSubmit();
+              }}
             >
               Submit
             </button>
-            {!formik.isValid && (
+            {!formik.isValid && hasSubmitted && (
               <div className={styles.formError}>
                 <FaExclamationCircle /> {errorArray[0]}
               </div>
--- src/routes/user/login/components/validation.ts
+++ src/routes/user/login/components/validation.ts
@@ -1,16 +1,19 @@
 import * as yup from 'yup';
 
 export const signUpValidationSchema = yup.object().shape({
-  userID: yup.string().required('User ID is required'),
+  userID: yup
+    .string()
+    .required('User ID is required')
+    .matches(/^[a-zA-Z0-9_]*$/, 'Only alphanumeric characters are allowed'),
+  email: yup
+    .string()
+    .email('Must be a valid email')
+    .required('Email is required'),
   password: yup.string().required('Password is required'),
   passwordConfirm: yup
     .string()
     .required('You must confirm your password')
-    .oneOf([yup.ref('password'), null], 'Passwords must match'),
-  email: yup
-    .string()
-    .email('Must be a valid email')
-    .required('Email is required')
+    .oneOf([yup.ref('password'), null], 'Passwords must match')
 });
 
 export const forgottenPasswordValidaitonSchema = yup.object().shape({
