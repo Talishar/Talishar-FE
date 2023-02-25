c039356eec010caf3230d678311695123f10f8e3
Stuart Myers <LaustinSpayce@users.noreply.github.com>
2023-02-25 20:59:05 +0800

    generator

--- scripts/card-generator.js
+++ scripts/card-generator.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 
 const url =
   'https://raw.githubusercontent.com/the-fab-cube/flesh-and-blood-cards/outsiders/json/english/card.json';
-const outputFile = 'src/constants/cardList.json';
+const outputFile = 'src/constants/cardList.ts';
 
 rp(url)
   .then((jsonString) => {
@@ -14,17 +14,26 @@ rp(url)
       const name = card.name;
       const id = printing.id;
 
-      if (!acc[name]) {
-        acc[name] = id;
+      if (!acc.has(name)) {
+        acc.set(name, id);
       }
 
       return acc;
-    }, {});
+    }, new Map());
+
+    const exportData = `export const CardList = new Map<string, string>(\n${indent(
+      JSON.stringify([...data]),
+      2
+    )}\n);`;
 
     fs.mkdirSync('../src/constants', { recursive: true }); // create the constants directory if it doesn't exist
-    fs.writeFileSync(outputFile, JSON.stringify(data, null, 2));
-    console.log('written card list to file');
+    fs.writeFileSync(outputFile, exportData);
   })
   .catch((err) => {
     console.error(err);
   });
+
+function indent(str, spaces) {
+  const padding = ' '.repeat(spaces);
+  return str.replace(/^/gm, padding);
+}
