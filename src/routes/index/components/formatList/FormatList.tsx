285efcf34fada00563dc80c45b5917e116bc6e11
Stuart Myers <LaustinSpayce@users.noreply.github.com>
2023-03-04 15:16:26 +0800

    Add Outsider heroes to filter

--- src/routes/index/components/formatList/FormatList.tsx
+++ src/routes/index/components/formatList/FormatList.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import styles from './FormatList.module.css';
 import { IOpenGame } from '../gameList/GameList';
 import OpenGame from '../openGame/OpenGame';
+import { useAutoAnimate } from '@formkit/auto-animate/react';
 
 export interface IFormatList {
   gameList: IOpenGame[];
@@ -9,12 +10,13 @@ export interface IFormatList {
   isOther?: boolean;
 }
 const FormatList = ({ gameList, name, isOther }: IFormatList) => {
+  const [parent] = useAutoAnimate();
   if (gameList.length === 0) {
     return null;
   }
 
   return (
-    <div className={styles.groupDiv}>
+    <div className={styles.groupDiv} ref={parent}>
       <h5 className={styles.subSectionTitle}>{name}</h5>
       {gameList.map((entry, ix: number) => {
         return (
