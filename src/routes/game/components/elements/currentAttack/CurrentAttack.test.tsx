0da8e9ee456f6672e8fa1f03a504e484a57bf2a4
Stuart Myers <LaustinSpayce@users.noreply.github.com>
2023-03-05 19:21:25 +0800

    add tests

--- src/routes/game/components/elements/currentAttack/CurrentAttack.test.tsx
+++ src/routes/game/components/elements/currentAttack/CurrentAttack.test.tsx
@@ -3,6 +3,11 @@ import { Provider } from 'react-redux';
 import { store } from 'app/Store';
 import { createRoot } from 'react-dom/client';
 import CurrentAttack from './CurrentAttack';
+import { vi } from 'vitest';
+import { screen, act, fireEvent } from '@testing-library/react';
+import { OfflineTestingGameState } from 'features/game/InitialGameState';
+import { renderWithProviders } from 'utils/TestUtils';
+import { useDispatch } from 'react-redux';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
@@ -12,4 +17,49 @@ it('renders without crashing', () => {
       <CurrentAttack />
     </Provider>
   );
+});
+
+const redux = { useDispatch };
+
+// only loading
+it('will show the current attack with attack and defence values', async () => {
+  renderWithProviders(<CurrentAttack />, {
+    preloadedState: { game: OfflineTestingGameState, auth: {} }
+  });
+
+  const attackValue = await screen.findByTestId('attack-value');
+  expect(attackValue).toMatchInlineSnapshot(`
+    <div
+      class="_attDiv_c76636"
+      data-testid="attack-value"
+    >
+      88
+    </div>
+  `);
+
+  const defenceValue = await screen.findByTestId('defence-value');
+  expect(defenceValue).toMatchInlineSnapshot(`
+    <div
+      class="_defDiv_c76636"
+      data-testid="defence-value"
+    >
+      44
+    </div>
+  `);
+});
+
+// TODO: learn how to do mocks properly
+it.skip('will try to open attack window when clicking attack img', async () => {
+  renderWithProviders(<CurrentAttack />, {
+    preloadedState: { game: OfflineTestingGameState, auth: {} }
+  });
+  const attackZoneDisplay = vi.spyOn(redux, 'useDispatch');
+
+  const attackSymbol = await screen.findByAltText('attack symbol');
+
+  act(() => {
+    attackSymbol.click();
+  });
+
+  expect(attackZoneDisplay).toBeCalled();
 });
