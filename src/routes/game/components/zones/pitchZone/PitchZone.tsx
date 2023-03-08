2f2209bd87d72d43daf74636977005fbead4037b
Stuart Myers <LaustinSpayce@users.noreply.github.com>
2023-03-08 13:09:12 +0800

    The stack is even animated

--- src/routes/game/components/zones/pitchZone/PitchZone.tsx
+++ src/routes/game/components/zones/pitchZone/PitchZone.tsx
@@ -6,6 +6,7 @@ import { setCardListFocus } from 'features/game/GameSlice';
 import styles from './PitchZone.module.css';
 import PitchDisplay from '../../elements/pitchDisplay/PitchDisplay';
 import { useAppDispatch, useAppSelector } from 'app/Hooks';
+import { motion, AnimatePresence } from 'framer-motion';
 
 export default function PitchZone(prop: Displayrow) {
   const { isPlayer } = prop;
@@ -40,13 +41,29 @@ export default function PitchZone(prop: Displayrow) {
     );
   };
 
+  const pitchOrder = [...pitchZone].reverse();
   const numInPitch = pitchZone.length;
   const cardToDisplay = { ...pitchZone[numInPitch - 1], borderColor: '' };
 
-  // TODO: Have the nice stacking effect like it was requested on twitter
   return (
     <div className={styles.pitchZone} onClick={pitchZoneDisplay}>
-      <CardDisplay card={cardToDisplay} num={0} preventUseOnClick />
+      <AnimatePresence>
+        {pitchOrder.map((card, ix) => {
+          return (
+            <motion.div
+              style={{ top: `-${2 * ix}em`, zIndex: `-${ix + 1}` }}
+              className={styles.pitchCard}
+              initial={{ y: `${2 * ix}em` }}
+              animate={{ y: 0 }}
+              transition={{ ease: 'easeIn', duration: 0.2 }}
+              exit={{ opacity: 0 }}
+              key={`${card.cardNumber}-${ix}`}
+            >
+              <CardDisplay card={card} num={ix} preventUseOnClick />
+            </motion.div>
+          );
+        })}
+      </AnimatePresence>
       <PitchDisplay isPlayer={isPlayer} DisplayRow={DisplayRow} />
     </div>
   );
