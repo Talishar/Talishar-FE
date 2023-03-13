d23093ff2825dd24dfce5e525049e6d58f144d4c
Stuart Myers <LaustinSpayce@users.noreply.github.com>
2023-03-13 23:16:20 +0800

    fix up test stuff

--- src/routes/game/components/zones/LandmarkZone/LandmarkZone.test.tsx
+++ src/routes/game/components/zones/LandmarkZone/LandmarkZone.test.tsx
@@ -4,12 +4,12 @@ import { store } from 'app/Store';
 import { createRoot } from 'react-dom/client';
 import LandmarkZone from './LandmarkZone';
 
-it('renders without crashing isPlayer true', () => {
+it('renders without crashing', () => {
   const div = document.createElement('div');
   const root = createRoot(div);
   root.render(
     <Provider store={store}>
-      <LandmarkZone isPlayer DisplayRow="" />
+      <LandmarkZone />
     </Provider>
   );
 });
--- src/routes/game/components/zones/LandmarkZone/LandmarkZone.tsx
+++ src/routes/game/components/zones/LandmarkZone/LandmarkZone.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { useAppSelector } from 'app/Hooks';
 import { RootState } from 'app/Store';
-import Displayrow from 'interface/Displayrow';
 import CardDisplay from '../../elements/cardDisplay/CardDisplay';
 import styles from './LandmarkZone.module.css';
 import { shallowEqual } from 'react-redux';
