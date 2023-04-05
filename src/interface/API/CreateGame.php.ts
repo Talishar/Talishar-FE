b2d6af01fcdbedf67c9d0977dbf617ebdc09a404
Stuart Myers <LaustinSpayce@users.noreply.github.com>
2023-04-05 21:42:31 +0800

    Remove zone counts

--- src/interface/API/CreateGame.php.ts
+++ src/interface/API/CreateGame.php.ts
@@ -2,7 +2,7 @@ export interface CreateGameAPI {
   deck?: string; // This is for limited game modes (see JoinGameInput.php)
   fabdb?: string; // Deck builder decklink (any deckbuilder, name comes from when fabdb was the only one)
   deckTestMode?: boolean; // If this is populated with ANYTHING, will start a game against the combat dummy
-  format?: string; // Format of the game enum
+  format?: string | number; // Format of the game enum (can also be a number)
   visibility?: string; // "public" = public game, "private" = private game
   decksToTry?: string; // This is only used if there's no favorite deck or decklink. 1 = ira
   favoriteDeck?: boolean; //Set this to true to save the provided deck link to your favorites
