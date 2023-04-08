379c251c04b0e0ae59acaa46724c9b8e086e8faa
Stuart Myers <LaustinSpayce@users.noreply.github.com>
2023-04-08 22:08:09 +0800

    tidy up

--- src/routes/game/components/elements/endGameMenuOptions/EndGameMenuOptions.tsx
+++ src/routes/game/components/elements/endGameMenuOptions/EndGameMenuOptions.tsx
@@ -4,7 +4,6 @@ import styles from './EndGameMenuOptions.module.css';
 import { useState } from 'react';
 import { PROCESS_INPUT } from 'appConstants';
 import { RootState } from 'app/Store';
-import Player from 'interface/Player';
 import { useNavigate } from 'react-router-dom';
 import { shallowEqual } from 'react-redux';
 import { getGameInfo } from 'features/game/GameSlice';
@@ -12,17 +11,12 @@ import { getGameInfo } from 'features/game/GameSlice';
 const EndGameMenuOptions = () => {
   const dispatch = useAppDispatch();
   const navigate = useNavigate();
-  const [hasRated, setHasRated] = useState(false);
-  const { roguelikeGameID } = useAppSelector(
-    getGameInfo,
-    shallowEqual
-  );
+  const { roguelikeGameID } = useAppSelector(getGameInfo, shallowEqual);
   //Always player 1 in roguelike, which is only place this matters
-  const health = useAppSelector((state: RootState) =>
-    state.game.playerOne.Health
+  const health = useAppSelector(
+    (state: RootState) => state.game.playerOne.Health ?? '0'
   );
 
-  // TODO: Need constants for the button modes.
   const handleMainMenu = async () => {
     dispatch(submitButton({ button: { mode: PROCESS_INPUT.MAIN_MENU } }));
     navigate('/');
@@ -38,7 +32,11 @@ const EndGameMenuOptions = () => {
   };
 
   const handleContinueAdventure = () => {
-    dispatch(submitButton({ button: { mode: PROCESS_INPUT.ROGUELIKE_RESUME_ADVENTURE } }));
+    dispatch(
+      submitButton({
+        button: { mode: PROCESS_INPUT.ROGUELIKE_RESUME_ADVENTURE }
+      })
+    );
   };
 
   return (
@@ -47,27 +45,21 @@ const EndGameMenuOptions = () => {
         <div className={styles.buttonDiv} onClick={handleMainMenu}>
           Main Menu
         </div>
-        {
-          roguelikeGameID != null && health > 0 ?
+        {roguelikeGameID && health > 0 && (
           <div className={styles.buttonDiv} onClick={handleContinueAdventure}>
             Continue Adventure
           </div>
-          : null
-        }
-        {
-            roguelikeGameID == null ?
+        )}
+        {!roguelikeGameID && (
+          <>
             <div className={styles.buttonDiv} onClick={handleQuickRematch}>
               Quick Rematch (no sideboarding)
             </div>
-            : null
-        }
-        {
-            roguelikeGameID == null ?
             <div className={styles.buttonDiv} onClick={handleFullRematch}>
               Full Rematch (new sideboard)
             </div>
-            : null
-        }
+          </>
+        )}
       </div>
     </div>
   );
