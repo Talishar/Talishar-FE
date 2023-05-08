2cb5194744a4e0d6d7945a70f37eb366393e96c5
Stuart Myers <LaustinSpayce@users.noreply.github.com>
2023-05-08 13:02:48 +0800

    set n forget cookie for experimental features

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { store } from './app/Store';
 import reportWebVitals from './reportWebVitals';
+import { CookiesProvider } from 'react-cookie';
 import './index.scss';
 import { Toaster } from 'react-hot-toast';
 import { router } from 'routes';
@@ -13,20 +14,22 @@ const container = document.getElementById('root')!;
 const root = createRoot(container);
 
 root.render(
-  <Provider store={store}>
-    <Toaster
-      position="top-left"
-      toastOptions={{
-        style: {
-          background: 'var(--dark-red)',
-          color: 'var(--white)',
-          border: '1px solid var(--primary)',
-          padding: '0.5rem'
-        }
-      }}
-    />
-    <RouterProvider router={router} />
-  </Provider>
+  <CookiesProvider>
+    <Provider store={store}>
+      <Toaster
+        position="top-left"
+        toastOptions={{
+          style: {
+            background: 'var(--dark-red)',
+            color: 'var(--white)',
+            border: '1px solid var(--primary)',
+            padding: '0.5rem'
+          }
+        }}
+      />
+      <RouterProvider router={router} />
+    </Provider>
+  </CookiesProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
