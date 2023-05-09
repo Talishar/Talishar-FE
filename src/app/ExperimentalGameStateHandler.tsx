98f60f2d282a484821bde0acff7031775795a0c3
Stuart Myers <LaustinSpayce@users.noreply.github.com>
2023-05-09 23:32:23 +0800

    Fix for spectate

--- src/app/ExperimentalGameStateHandler.tsx
+++ src/app/ExperimentalGameStateHandler.tsx
@@ -1,17 +1,30 @@
 import React, { useEffect } from 'react';
 import { useCookies } from 'react-cookie';
-import { useParams } from 'react-router-dom';
+import { useLocation, useParams } from 'react-router-dom';
 import { useAppDispatch, useAppSelector } from './Hooks';
-import { getGameInfo, nextTurn } from 'features/game/GameSlice';
+import { getGameInfo, nextTurn, setGameStart } from 'features/game/GameSlice';
 import { shallowEqual } from 'react-redux';
+import { useKnownSearchParams } from 'hooks/useKnownSearchParams';
+import { GameLocationState } from 'interface/GameLocationState';
 
 const ExperimentalGameStateHandler = () => {
   const [cookies] = useCookies(['experimental']);
   const { gameID } = useParams();
   const gameInfo = useAppSelector(getGameInfo, shallowEqual);
   const dispatch = useAppDispatch();
+  const [{ gameName = '0', playerID = '3', authKey = '' }] =
+    useKnownSearchParams();
+  const location = useLocation();
+  const locationState = location.state as GameLocationState | undefined;
 
   useEffect(() => {
+    dispatch(
+      setGameStart({
+        gameID: parseInt(gameID ?? gameName),
+        playerID: locationState?.playerID ?? parseInt(playerID),
+        authKey: gameInfo.authKey || authKey
+      })
+    );
     console.log('setting up listener');
     const source = new EventSource(
       `https://api.talishar.net/game/GetUpdateSSE.php?gameName=${gameID}&playerID=${gameInfo.playerID}&authKey=${gameInfo.authKey}`
@@ -21,7 +34,14 @@ const ExperimentalGameStateHandler = () => {
       console.log('update data:', e.data);
       dispatch(
         nextTurn({
-          game: gameInfo,
+          game: {
+            gameID: gameInfo.gameID || parseInt(gameID ?? gameName),
+            playerID:
+              gameInfo.playerID ||
+              (locationState?.playerID ?? parseInt(playerID)),
+            authKey: gameInfo.authKey || authKey,
+            isPrivate: gameInfo.isPrivate
+          },
           signal: undefined,
           lastUpdate: 0
         })
