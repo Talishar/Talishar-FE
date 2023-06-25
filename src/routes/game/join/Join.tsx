ef82e70e93289baf6be8300e7e1af61eaac24658
Stuart Myers <LaustinSpayce@users.noreply.github.com>
2023-06-25 22:03:48 +0800

    refactor to react hook form

--- src/routes/game/join/Join.tsx
+++ src/routes/game/join/Join.tsx
@@ -1,10 +1,9 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useAppDispatch } from 'app/Hooks';
 import {
   useGetFavoriteDecksQuery,
   useJoinGameMutation
 } from 'features/api/apiSlice';
-import { Formik, Form, Field, FormikHelpers } from 'formik';
 import { JoinGameAPI } from 'interface/API/JoinGame.php';
 import { Link, useNavigate, useParams } from 'react-router-dom';
 import styles from './Join.module.css';
@@ -12,10 +11,12 @@ import { useKnownSearchParams } from 'hooks/useKnownSearchParams';
 import { setGameStart } from 'features/game/GameSlice';
 import { toast } from 'react-hot-toast';
 import { FaExclamationCircle } from 'react-icons/fa';
-import CreateGameErrors from '../create/CreateGameErrors';
 import validationSchema from './validationSchema';
 import useAuth from 'hooks/useAuth';
 import classNames from 'classnames';
+import { SubmitHandler, useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import { ErrorMessage } from '@hookform/error-message';
 
 const JoinGame = () => {
   const navigate = useNavigate();
@@ -29,6 +30,17 @@ const JoinGame = () => {
 
   const { gameID } = useParams();
   gameName = gameID ?? gameName;
+
+  const {
+    formState: { isSubmitting, errors },
+    register,
+    handleSubmit,
+    setError,
+    reset
+  } = useForm<JoinGameAPI>({
+    mode: 'onBlur',
+    resolver: yupResolver(validationSchema)
+  });
 
   const initialValues: JoinGameAPI = {
     deck: '',
@@ -45,15 +57,15 @@ const JoinGame = () => {
     gameDescription: ''
   };
 
-  const handleSubmit = async (
-    values: JoinGameAPI,
-    { setSubmitting }: FormikHelpers<JoinGameAPI>
-  ) => {
+  useEffect(() => {
+    reset(initialValues);
+  }, [initialValues, reset]);
+
+  const onSubmit: SubmitHandler<JoinGameAPI> = async (values: JoinGameAPI) => {
     values.playerID = parseInt(playerID);
     values.gameName = parseInt(gameName);
 
     try {
-      setSubmitting(true);
       const response = await joinGame(values).unwrap();
       if (response.error) {
         throw response.error;
@@ -73,82 +85,98 @@ const JoinGame = () => {
     } catch (error) {
       console.warn(error);
       toast.error(String(error), { position: 'top-center' });
-    } finally {
-      setSubmitting(false);
+      setError('root.serverError', {
+        type: 'custom',
+        message: `There has been an error while joining the game. Message: ${JSON.stringify(
+          error
+        )} Please try again`
+      });
     }
   };
 
   return (
     <main className={styles.LoginPageContainer}>
       <article className={styles.formContainer}>
-        <Formik
-          initialValues={initialValues}
-          onSubmit={handleSubmit}
-          validationSchema={validationSchema}
-        >
-          {({ isSubmitting }) => (
-            <Form>
-              <div className={styles.form}>
-                {isLoggedIn && !isLoading && (
-                  <label>
-                    Favorite Deck
-                    <Field
-                      as="select"
-                      name="favoriteDecks"
-                      id="favoriteDecks"
-                      placeholder="Select a favorite deck"
-                      aria-busy={isLoading}
-                    >
-                      {data?.favoriteDecks.map((deck, ix) => (
-                        <option value={deck.key} key={deck.index}>
-                          {deck.name}
-                        </option>
-                      ))}
-                    </Field>
-                  </label>
-                )}
-                <fieldset>
-                  <label>
-                    Deck Link:
-                    <Field
-                      type="text"
-                      id="fabdb"
-                      name="fabdb"
-                      aria-label="Deck Link"
-                    />
-                  </label>
-                  {isLoggedIn && (
-                    <label>
-                      <Field
-                        type="checkbox"
-                        role="switch"
-                        id="favoriteDeck"
-                        name="favoriteDeck"
-                      />
-                      Save Deck to ❤️ Favorites
-                    </label>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <div className={styles.form}>
+            {isLoggedIn && !isLoading && (
+              <label>
+                Favorite Deck
+                <select
+                  id="favoriteDecks"
+                  aria-busy={isLoading}
+                  {...register('favoriteDecks')}
+                  aria-invalid={
+                    errors.favoriteDecks?.message ? 'true' : undefined
+                  }
+                >
+                  {data?.favoriteDecks.map((deck, ix) => (
+                    <option value={deck.key} key={deck.index}>
+                      {deck.name}
+                    </option>
+                  ))}
+                </select>
+                <ErrorMessage
+                  errors={errors}
+                  name="favoriteDecks"
+                  render={({ message }) => <p>{message}</p>}
+                />
+              </label>
+            )}
+            <fieldset>
+              <label>
+                Deck Link:
+                <input
+                  type="text"
+                  id="fabdb"
+                  aria-label="Deck Link"
+                  {...register('fabdb')}
+                  aria-invalid={errors.deck?.message ? 'true' : undefined}
+                />
+                <ErrorMessage
+                  errors={errors}
+                  name="fabdb"
+                  render={({ message }) => (
+                    <p className={styles.fieldError}>
+                      <FaExclamationCircle /> {message}
+                    </p>
                   )}
-                </fieldset>
-              </div>
-              {!isLoggedIn && (
-                <p>
-                  <small>
-                    You must be <Link to="/user/login">logged in</Link> to join
-                    public lobbies.
-                  </small>
-                </p>
+                />
+              </label>
+              {isLoggedIn && (
+                <label>
+                  <input
+                    type="checkbox"
+                    role="switch"
+                    id="favoriteDeck"
+                    {...register('favoriteDeck')}
+                  />
+                  Save Deck to ❤️ Favorites
+                </label>
               )}
-              <button
-                type="submit"
-                className={styles.buttonClass}
-                aria-busy={isSubmitting}
-              >
-                Join Game
-              </button>
-              <CreateGameErrors createGameResult={joinGameResult.data} />
-            </Form>
+            </fieldset>
+          </div>
+          {!isLoggedIn && (
+            <p>
+              <small>
+                You must be <Link to="/user/login">logged in</Link> to join
+                public lobbies.
+              </small>
+            </p>
           )}
-        </Formik>
+          <button
+            type="submit"
+            className={styles.buttonClass}
+            aria-busy={isSubmitting}
+          >
+            Join Game
+          </button>
+          {errors.root?.serverError?.message && (
+            <div className={styles.fieldError}>
+              <FaExclamationCircle /> {errors.root?.serverError?.message}
+            </div>
+          )}
+        </form>
         <hr />
         <button
           style={{ marginTop: '27px' }}
