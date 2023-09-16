ae52e5de6ff969e5719468a00e0df349228849b8
Stuart Myers <LaustinSpayce@users.noreply.github.com>
2023-09-16 21:44:32 +0800

    grabby hand

--- src/routes/game/components/combatChain/CombatChain.tsx
+++ src/routes/game/components/combatChain/CombatChain.tsx
@@ -3,20 +3,42 @@ import styles from './CombatChain.module.css';
 import ChainLinks from '../elements/chainLinks/ChainLinks';
 import CurrentAttack from '../elements/currentAttack/CurrentAttack';
 import Reactions from '../elements/reactions/Reactions';
-import TurnWidget from '../elements/turnWidget/TurnWidget';
-import ExperimentalTurnWidget from '../elements/experimentalTurnWidget/ExperimentalTurnWidget';
 import { useCookies } from 'react-cookie';
+import { useAppSelector } from '../../../../app/Hooks';
+import { RootState } from 'app/Store';
+import { motion, AnimatePresence } from 'framer-motion';
 
 export default function CombatChain() {
+  const oldCombatChain =
+    useAppSelector((state: RootState) => state.game.oldCombatChain) ?? [];
+  const activeCombatChain = useAppSelector(
+    (state: RootState) => state.game.activeChainLink
+  );
   const [cookies] = useCookies(['experimental']);
+
+  const showCombatChain =
+    oldCombatChain?.length > 0 ||
+    (activeCombatChain?.attackingCard &&
+      activeCombatChain?.attackingCard?.cardNumber !== 'blank');
   return (
-    <div className={styles.combatChain}>
-      <CurrentAttack />
-      <div className={styles.chainCentre}>
-        <ChainLinks />
-        <Reactions />
-      </div>
-      {cookies.experimental ? <ExperimentalTurnWidget /> : <TurnWidget />}
-    </div>
+    <AnimatePresence>
+      {showCombatChain && (
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          exit={{ opacity: 0 }}
+          className={styles.combatChain}
+          drag="y"
+          dragConstraints={{ top: -300, bottom: 300 }}
+        >
+          <div className={styles.grabbyHandle}></div>
+          <CurrentAttack />
+          <div className={styles.chainCentre}>
+            <ChainLinks />
+            <Reactions />
+          </div>
+        </motion.div>
+      )}
+    </AnimatePresence>
   );
 }
--- src/routes/game/components/gridBoard/GridBoard.tsx
+++ src/routes/game/components/gridBoard/GridBoard.tsx
@@ -19,103 +19,110 @@ import PlayerPrompt from '../elements/playerPrompt/PlayerPrompt';
 import Playmat from '../elements/playmat';
 
 import styles from './GridBoard.module.css';
+import { useCookies } from 'react-cookie';
+import ExperimentalTurnWidget from '../elements/experimentalTurnWidget';
+import TurnWidget from '../elements/turnWidget/TurnWidget';
 
 const GridBoard = () => {
+  const [cookies] = useCookies(['experimental']);
   return (
-    <div className={styles.gameBoardGrid}>
-      <div className={styles.pTwoFeet}>
-        <FeetEqZone isPlayer={false} />
-      </div>
-      <div className={styles.pTwoPermanents}>
-        <PermanentsZone isPlayer={false} />
-      </div>
-      <div className={styles.pTwoArsenal}>
-        <ArsenalZone isPlayer={false} />
-      </div>
-      <div className={styles.pTwoCount}>
-        <ZoneCounts isPlayer={false} />
-      </div>
-      <div className={styles.pTwoBanish}>
-        <BanishZone isPlayer={false} />
-      </div>
-      <div className={styles.pTwoChest}>
-        <ChestEqZone isPlayer={false} />
-      </div>
-      <div className={styles.pTwoHands}>
-        <GlovesEqZone isPlayer={false} />
-      </div>
-      <div className={styles.pTwoWeaponLZone}>
-        <WeaponLZone isPlayer={false} />
-      </div>
-      <div className={styles.pTwoHero}>
-        <HeroZone isPlayer={false} />
-      </div>
-      <div className={styles.pTwoWeaponRZone}>
-        <WeaponRZone isPlayer={false} />
-      </div>
-      <div className={styles.pTwoPitch}>
-        <PitchZone isPlayer={false} />
-      </div>
-      <div className={styles.pTwoDeck}>
-        <DeckZone isPlayer={false} />
-      </div>
-      <div className={styles.pTwoHead}>
-        <HeadEqZone isPlayer={false} />
-      </div>
-      <div className={styles.pTwoGraveyard}>
-        <GraveyardZone isPlayer={false} />
-      </div>
-      <>
+    <>
+      <div className={styles.gameBoardGrid}>
+        <div className={styles.pTwoFeet}>
+          <FeetEqZone isPlayer={false} />
+        </div>
+        <div className={styles.pTwoPermanents}>
+          <PermanentsZone isPlayer={false} />
+        </div>
+        <div className={styles.pTwoArsenal}>
+          <ArsenalZone isPlayer={false} />
+        </div>
+        <div className={styles.pTwoCount}>
+          <ZoneCounts isPlayer={false} />
+        </div>
+        <div className={styles.pTwoBanish}>
+          <BanishZone isPlayer={false} />
+        </div>
+        <div className={styles.pTwoChest}>
+          <ChestEqZone isPlayer={false} />
+        </div>
+        <div className={styles.pTwoHands}>
+          <GlovesEqZone isPlayer={false} />
+        </div>
+        <div className={styles.pTwoWeaponLZone}>
+          <WeaponLZone isPlayer={false} />
+        </div>
+        <div className={styles.pTwoHero}>
+          <HeroZone isPlayer={false} />
+        </div>
+        <div className={styles.pTwoWeaponRZone}>
+          <WeaponRZone isPlayer={false} />
+        </div>
+        <div className={styles.pTwoPitch}>
+          <PitchZone isPlayer={false} />
+        </div>
+        <div className={styles.pTwoDeck}>
+          <DeckZone isPlayer={false} />
+        </div>
+        <div className={styles.pTwoHead}>
+          <HeadEqZone isPlayer={false} />
+        </div>
+        <div className={styles.pTwoGraveyard}>
+          <GraveyardZone isPlayer={false} />
+        </div>
+        <div className={styles.healthWidget}>
+          {cookies.experimental ? <ExperimentalTurnWidget /> : <TurnWidget />}
+        </div>
+        <div className={styles.pOneGraveyard}>
+          <GraveyardZone isPlayer={true} />
+        </div>
+        <div className={styles.pOneHead}>
+          <HeadEqZone isPlayer={true} />
+        </div>
+        <div className={styles.pOneDeck}>
+          <DeckZone isPlayer={true} />
+        </div>
+        <div className={styles.pOnePitch}>
+          <PitchZone isPlayer={true} />
+        </div>
+        <div className={styles.pOneWeaponRZone}>
+          <WeaponRZone isPlayer={true} />
+        </div>
+        <div className={styles.pOneHero}>
+          <HeroZone isPlayer={true} />
+        </div>
+        <div className={styles.pOneWeaponLZone}>
+          <WeaponLZone isPlayer={true} />
+        </div>
+        <div className={styles.pOneHands}>
+          <GlovesEqZone isPlayer={true} />
+        </div>
+        <div className={styles.pOneChest}>
+          <ChestEqZone isPlayer={true} />
+        </div>
+        <div className={styles.pOneBanish}>
+          <BanishZone isPlayer={true} />
+        </div>
+        <div className={styles.pOneCount}>
+          <ZoneCounts isPlayer={true} />
+        </div>
+        <div className={styles.pOneArsenal}>
+          <ArsenalZone isPlayer={true} />
+        </div>
+        <div className={styles.pOnePermanents}>
+          <PermanentsZone isPlayer={true} />
+        </div>
+        <div className={styles.pOneFeet}>
+          <FeetEqZone isPlayer={true} />
+        </div>
+        <Playmat isPlayer={false} />
+        <Playmat isPlayer={true} />
         <div className={styles.combatChain}>
           <CombatChain />
-          <PlayerPrompt />
+          {/* <PlayerPrompt /> */}
         </div>
-      </>
-      <div className={styles.pOneGraveyard}>
-        <GraveyardZone isPlayer={true} />
       </div>
-      <div className={styles.pOneHead}>
-        <HeadEqZone isPlayer={true} />
-      </div>
-      <div className={styles.pOneDeck}>
-        <DeckZone isPlayer={true} />
-      </div>
-      <div className={styles.pOnePitch}>
-        <PitchZone isPlayer={true} />
-      </div>
-      <div className={styles.pOneWeaponRZone}>
-        <WeaponRZone isPlayer={true} />
-      </div>
-      <div className={styles.pOneHero}>
-        <HeroZone isPlayer={true} />
-      </div>
-      <div className={styles.pOneWeaponLZone}>
-        <WeaponLZone isPlayer={true} />
-      </div>
-      <div className={styles.pOneHands}>
-        <GlovesEqZone isPlayer={true} />
-      </div>
-      <div className={styles.pOneChest}>
-        <ChestEqZone isPlayer={true} />
-      </div>
-      <div className={styles.pOneBanish}>
-        <BanishZone isPlayer={true} />
-      </div>
-      <div className={styles.pOneCount}>
-        <ZoneCounts isPlayer={true} />
-      </div>
-      <div className={styles.pOneArsenal}>
-        <ArsenalZone isPlayer={true} />
-      </div>
-      <div className={styles.pOnePermanents}>
-        <PermanentsZone isPlayer={true} />
-      </div>
-      <div className={styles.pOneFeet}>
-        <FeetEqZone isPlayer={true} />
-      </div>
-      <Playmat isPlayer={false} />
-      <Playmat isPlayer={true} />
-    </div>
+    </>
   );
 };
 export default GridBoard;
