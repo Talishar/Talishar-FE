a5bb3227870f464b52c2cba1c80d782c8e3208fa
PvtVoid <jotrickr@gmail.com>
2024-02-10 20:22:26 +0100

    Hid Hover when it's a cardback

--- src/routes/game/components/elements/cardPopUp/CardPopUp.tsx
+++ src/routes/game/components/elements/cardPopUp/CardPopUp.tsx
@@ -28,7 +28,9 @@ export default function CardPopUp({
   const [windowWidth, windowHeight] = useWindowDimensions();
 
   const handleMouseEnter = () => {
-    if (ref.current === null || isHidden === true || cardNumber === "ENDSTEP" || cardNumber === "ENDTURN" || cardNumber === "RESUMETURN" || cardNumber === "PHANTASM" || cardNumber === "MIRAGE" || cardNumber === "FINALIZECHAINLINK" || cardNumber === "DEFENDSTEP" || cardNumber === "ATTACKSTEP") {
+    if (ref.current === null || isHidden === true || cardNumber === "ENDSTEP" || cardNumber === "ENDTURN" || cardNumber === "RESUMETURN" 
+    || cardNumber === "PHANTASM" || cardNumber === "MIRAGE" || cardNumber === "FINALIZECHAINLINK" || cardNumber === "DEFENDSTEP" 
+    || cardNumber === "ATTACKSTEP" || cardNumber === "CardBack") {
       return;
     }
     const rect = ref.current.getBoundingClientRect();
