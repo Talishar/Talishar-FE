da09ca829f75c90e464455eafd68f3c62c4c32ec
PvtVoid <jotrickr@gmail.com>
2024-02-12 11:08:54 +0100

    CSS Change for "Are you sure you want to skip arsenal?" prompt

--- src/routes/game/components/elements/passTurnDisplay/PassTurnDisplay.tsx
+++ src/routes/game/components/elements/passTurnDisplay/PassTurnDisplay.tsx
@@ -86,8 +86,8 @@ export default function PassTurnDisplay() {
               <dialog open={showAreYouSureModal} className={styles.modal}>
                 <article>
                   <header>{preventPassPrompt}</header>
-                  <button onClick={clickYes}>YES</button>
-                  <button onClick={clickNo}>NO</button>
+                  <button className={styles.preventButtons} onClick={clickYes}>YES</button>
+                  <button className={styles.preventButtons} onClick={clickNo}>NO</button>
                 </article>
               </dialog>
             </>,
