89ae84b2b7356c6c57abde186f2d70f889d83410
PvtVoid <jotrickr@gmail.com>
2024-02-17 11:35:11 +0100

    Add back border colors for effects

--- src/routes/game/components/elements/effects/Effects.tsx
+++ src/routes/game/components/elements/effects/Effects.tsx
@@ -9,14 +9,17 @@ export interface CardProp {
   card: Card;
   num?: number;
   name?: string;
+  borderColor?: string;
 }
 
 export function Effect(prop: CardProp) {
   const src = `/crops/${prop.card.cardNumber}_cropped.png`;
 
+  const borderStyle = { "--effect-border-color": prop.card.borderColor } as React.CSSProperties;
+  
   return (
     <CardPopUp cardNumber={prop.card.cardNumber} containerClass={styles.effect}>
-      <img src={src} className={styles.img} />
+      <img src={src} className={styles.img} style={borderStyle}/>
     </CardPopUp>
   );
 }
