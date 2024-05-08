733952d84a7d968891b97e75bdddfa1ff4fa4716
PvtVoid <jotrickr@gmail.com>
2024-05-08 11:33:59 +0200

    Update Warning

--- src/routes/game/create/CreateGame.tsx
+++ src/routes/game/create/CreateGame.tsx
@@ -116,7 +116,7 @@ const CreateGame = () => {
       <article className={styles.formContainer}>
         <h3>Create New Game</h3>
         <p className={styles.fieldError}>
-          <FaExclamationCircle /> Warning - In 1h at 11:15 AM CET an update will be pushed to the live servers. The games in progress will crash and new games will be required.
+          <FaExclamationCircle /> Warning - SOON! an update will be pushed to the live servers. The games in progress will crash and new games will be required.
           </p>
         <p>
             <small className={styles.disclaimer}>
