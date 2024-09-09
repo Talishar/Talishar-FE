c8c833d843617360412c1645276b7296532f7f3f
PvtVoid <jotrickr@gmail.com>
2024-09-09 12:17:37 +0200

    Updated cardlist

--- scripts/card-generator.js
+++ scripts/card-generator.js
@@ -1,7 +1,7 @@
 const axios = require('axios');
 const fs = require('fs');
 
-const url = 'https://raw.githubusercontent.com/the-fab-cube/flesh-and-blood-cards/part-the-mistveil/json/english/card.json'; //MST
+const url = 'https://raw.githubusercontent.com/the-fab-cube/flesh-and-blood-cards/rosetta/json/english/card.json'; //ROS
 //const url = 'https://raw.githubusercontent.com/the-fab-cube/flesh-and-blood-cards/develop/json/english/card.json'; //AKO
 
 
