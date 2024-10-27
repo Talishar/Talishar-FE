d7f0107cf2eabe966a0ce8d690ca03f0a05e4cd5
PvtVoid <jotrickr@gmail.com>
2024-10-27 20:10:24 +0100

    Undo Fix shortcut preventing the user to type in the menu/profile

--- src/hooks/useShortcut.tsx
+++ src/hooks/useShortcut.tsx
@@ -1,28 +1,18 @@
 import { useCallback } from 'react';
-import { useState, useEffect } from 'react';
-import { useLocation } from 'react-router-dom';
-import { BACKEND_URL } from 'appConstants';
 
 
 const shortcutListeners = new Map<string, (event: KeyboardEvent) => void>();
 
 const useShortcut = (keyCode: string, callback: Function) => {
-  const location = useLocation();
-  const [isEnabled, setIsEnabled] = useState(false);
-
-  useEffect(() => {
-    const isEnabled = location.pathname !== BACKEND_URL && !location.pathname.startsWith('/user/profile');
-    setIsEnabled(isEnabled);
-  }, [location.pathname]);
 
   const handleKeyPress = useCallback(
     (event: KeyboardEvent) => {
-      if (isEnabled && event.code === keyCode) {
+      if (event.code === keyCode) {
         callback(event);
         event.preventDefault(); // prevent default behavior
       }
     },
-    [keyCode, callback, isEnabled]
+    [keyCode, callback]
   );
 
   if (!shortcutListeners.has(keyCode)) {
