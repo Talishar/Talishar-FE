bcba04bd1975f00f789d7996fcb9006130993672
PvtVoid <jotrickr@gmail.com>
2025-04-28 10:21:17 +0200

    Update LocalKeyManagement.ts

--- src/utils/LocalKeyManagement.ts
+++ src/utils/LocalKeyManagement.ts
@@ -1,8 +1,17 @@
 export const saveGameAuthKey = (gameId: number, authKey: string): void => {
+  if (gameId <= 0) {
+    console.error('Invalid game ID, cannot save authKey for game:', gameId);
+    return;
+  }
   sessionStorage.setItem(String(gameId), authKey);
 };
 
 export const loadGameAuthKey = (gameId: number): string => {
+  if (gameId <= 0) {
+    console.error('Invalid game ID, cannot load authKey for game:', gameId);
+    return '';
+  }
+
   const localValue = sessionStorage.getItem(String(gameId));
   if (!localValue) {
     console.error(
@@ -14,11 +23,16 @@ export const loadGameAuthKey = (gameId: number): string => {
 };
 
 export const deleteGameAuthKey = (gameId: number): void => {
+  if (gameId <= 0) {
+    console.error('Invalid game ID, cannot delete authKey for game:', gameId);
+    return;
+  }
+
   const valueExists = sessionStorage.getItem(String(gameId));
   if (!valueExists) {
     console.error(
       'Attempted to delete a authKey of a game that does not exist, game',
-      +gameId
+      gameId
     );
     return;
   }
