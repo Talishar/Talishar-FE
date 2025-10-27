a24f22fd00fda98876cd5e8a4f912e8f83931325
PvtVoid <jotrickr@gmail.com>
2025-10-27 21:51:18 +0100

    Fix new modPage errors

--- src/features/api/apiSlice.ts
+++ src/features/api/apiSlice.ts
@@ -379,8 +379,8 @@ export const apiSlice = createApi({
       query: ({ ipToBan, playerNumberToBan }) => {
         return {
           url: URL_END_POINT.BAN_PLAYER,
-          method: 'GET',
-          params: {
+          method: 'POST',
+          body: {
             ipToBan: ipToBan,
             playerNumberToBan: playerNumberToBan
           },
@@ -392,8 +392,8 @@ export const apiSlice = createApi({
       query: ({ playerToBan }) => {
         return {
           url: URL_END_POINT.BAN_PLAYER,
-          method: 'GET',
-          params: {
+          method: 'POST',
+          body: {
             playerToBan: playerToBan
           },
           responseHandler: parseResponse
@@ -404,8 +404,8 @@ export const apiSlice = createApi({
       query: ({ gameToClose }) => {
         return {
           url: URL_END_POINT.CLOSE_GAME,
-          method: 'GET',
-          params: {
+          method: 'POST',
+          body: {
             gameToClose: gameToClose
           },
           responseHandler: parseResponse
