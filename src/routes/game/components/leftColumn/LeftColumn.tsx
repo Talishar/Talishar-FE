28d34d0ad9fdd24e0ef06d1e6f2fd3f7eef9ca43
PvtVoid <jotrickr@gmail.com>
2025-11-10 08:57:34 +0100

    Add player names to mobile while spectating

--- src/routes/game/components/leftColumn/LeftColumn.tsx
+++ src/routes/game/components/leftColumn/LeftColumn.tsx
@@ -3,10 +3,18 @@ import ActiveEffects from '../activeEffects/ActiveEffects';
 import PlayerName from '../elements/playerName/PlayerName';
 import DevToolPanel from './DevToolPanel/DevToolPanel';
 import styles from './LeftColumn.module.css';
+import { useAppSelector } from 'app/Hooks';
+import { shallowEqual } from 'react-redux';
+import { getGameInfo } from 'features/game/GameSlice';
 
 export default function LeftColumn() {
 
   const [isMobile, setIsMobile] = useState(false);
+  const { playerID } = useAppSelector(getGameInfo, shallowEqual);
+
+  useEffect(() => {
+    console.log('LeftColumn - playerID:', playerID, 'isMobile:', isMobile);
+  }, [playerID, isMobile]);
 
   useEffect(() => {
     const handleResize = () => {
@@ -27,8 +35,8 @@ export default function LeftColumn() {
         <PlayerName isPlayer={false} />
       )}
       <ActiveEffects />
-      {!isMobile && (
-        <PlayerName isPlayer />
+      {(!isMobile || playerID === 3) && (
+        <PlayerName isPlayer={true} />
       )}
       <DevToolPanel />
     </div>
