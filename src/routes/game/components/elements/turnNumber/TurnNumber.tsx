cfd08b896288068067881953b16635e4373fd806
PvtVoid <jotrickr@gmail.com>
2026-03-10 08:05:40 +0100

    Hide opponent name in TurnNumber widget section with streamer mode

--- src/routes/game/components/elements/turnNumber/TurnNumber.tsx
+++ src/routes/game/components/elements/turnNumber/TurnNumber.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { useAppSelector } from 'app/Hooks';
 import { RootState } from 'app/Store';
 import styles from './TurnNumber.module.css';
+import useSetting from 'hooks/useSetting';
+import { IS_STREAMER_MODE } from 'features/options/constants';
 
 export default function TurnNumber() {
   let turnNumber = useAppSelector(
@@ -24,9 +26,13 @@ export default function TurnNumber() {
     (turnPlayer == 1 && amIPlayerOne || turnPlayer == 2 && !amIPlayerOne) ? state.game.playerOne.Name : state.game.playerTwo.Name
   );
 
+  const isStreamerMode = String(useSetting({ settingName: IS_STREAMER_MODE })?.value) === '1';
+  const isOpponentTurn = (turnPlayer == 1 && !amIPlayerOne) || (turnPlayer == 2 && amIPlayerOne);
+  const displayName = (isStreamerMode && isOpponentTurn) ? 'Opponent' : String(playerName ?? 'Unknown').substring(0, 15);
+
   return (
     <div className={styles.turnNumber}>
-      <div>{(String(playerName ?? "Unknown").substring(0, 15)) + "'s Turn"}</div>
+      <div>{displayName + "'s Turn"}</div>
       <div>Turn #{turnNumber}</div>
     </div>
   );
