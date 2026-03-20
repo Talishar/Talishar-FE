0a97041534fabda49f098341832c7d037fe3b663
PvtVoid <jotrickr@gmail.com>
2026-03-20 21:32:02 +0100

    More ads fixes

--- src/hooks/useAdScript.ts
+++ src/hooks/useAdScript.ts
@@ -1,8 +1,33 @@
 import { useEffect } from 'react';
 
+function purgeAdElements() {
+  document
+    .querySelectorAll('script[src*="rev.iq"]')
+    .forEach((el) => el.remove());
+
+  document
+    .querySelectorAll(
+      '[id^="rev-"], [class*="rev-content"], [class*="revcontent"],' +
+      'iframe[src*="rev.iq"], iframe[src*="revcontent"],' +
+      'div[data-ad], ins.adsbygoogle,' +
+      '[id*="google_ads"], [id*="gpt-ad"], [id*="aswift"],' +
+      'iframe[id*="google_ads"], iframe[src*="googlesyndication"],' +
+      'div[id*="adsense"], div[class*="google-ad"]'
+    )
+    .forEach((el) => el.remove());
+
+  try {
+    (window as any).adsbygoogle = { push: () => {} };
+  } catch (_) {
+  }
+}
+
 export default function useAdScript(enabled: boolean = true) {
   useEffect(() => {
-    if (!enabled) return;
+    if (!enabled) {
+      purgeAdElements();
+      return;
+    }
 
     const script = document.createElement('script');
     script.src = '//js.rev.iq/talishar.net';
@@ -10,12 +35,7 @@ export default function useAdScript(enabled: boolean = true) {
     document.head.appendChild(script);
 
     return () => {
-      // Remove any ad containers/iframes the Rev.IQ script appended to the DOM
-      document
-        .querySelectorAll(
-          '[id^="rev-"], [class*="rev-content"], [class*="revcontent"], iframe[src*="rev.iq"], iframe[src*="revcontent"], div[data-ad]'
-        )
-        .forEach((el) => el.remove());
+      purgeAdElements();
     };
   }, [enabled]);
 }
