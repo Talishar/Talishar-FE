73e02cf0dc06759a8ad1897d107dd65363fccdbd
PvtVoid <jotrickr@gmail.com>
2026-07-07 09:46:37 +0200

    CBOddwillows

--- src/features/options/cardBacks.ts
+++ src/features/options/cardBacks.ts
@@ -136,6 +136,7 @@ export const CARD_BACK = {
   '135': 'CBWingedHussars',
   '136': 'CBFabInsight',
   '137': 'CBNxi2',
+  '138': 'CBOddwillows',
 } as { [key: string]: string };
 
 /* Talishar supporter card back IDs - shown as locked previews to non-supporters */
