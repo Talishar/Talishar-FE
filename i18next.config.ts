cd950fe4d6a8d4560e55422d818fea08305acdb6
Alexander De Laurentiis <delaurentiisa99@gmail.com>
2026-08-03 16:26:21 +0000

    fixed a few translations

--- i18next.config.ts
+++ i18next.config.ts
@@ -9,7 +9,20 @@ export default defineConfig({
   ],
   extract: {
     input: "src/**/*.{js,jsx,ts,tsx}",
-    output: "public/locales/{{language}}/{{namespace}}.json"
-  }
-  
+    output: "public/locales/{{language}}/{{namespace}}.json",
+    // Primary language settings
+    primaryLanguage: 'en', // Defaults to the first locale in the `locales` array
+    removeUnusedKeys: true,
+    // Translation functions to detect. Defaults to ['t', '*.t'].
+    // Supports a leading wildcard to match any object (suffix match), e.g.
+    // '*.t' matches `i18n.t` / `this._i18n.t`, and a trailing wildcard to match
+    // any method on an object (prefix match), e.g. 'tProps.*' matches
+    // `tProps.label` / `tProps.title`.
+    functions: ['t'],  
+    // React components to analyze
+    transComponents: ['Trans'],
+    // Output formatting
+    sort: true, // can be also a sort function => i.e. (a, b) => a.key > b.key ? -1 : a.key < b.key ? 1 : 0, // sort in reverse order
+    indentation: 2, // can be also a string
+  },
 })
