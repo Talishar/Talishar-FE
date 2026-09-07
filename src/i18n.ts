1514cf1bb709d0cdf800c99fcd19da0384981c03
PvtVoid <jotrickr@gmail.com>
2026-09-07 22:10:03 +0200

    Fix translation error logs

--- src/i18n.ts
+++ src/i18n.ts
@@ -3,11 +3,6 @@ import { initReactI18next } from 'react-i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
 import Backend from 'i18next-http-backend';
 import { I18N_SUPPORTED_LANGUAGE_CODES } from './constants/i18nSupportedLanguages';
-import enTranslations from '../public/locales/en/translation.json';
-
-// the translations
-// (tip move them in a JSON file and import them,
-// or even better, manage them separated from your code: https://react.i18next.com/guides/multiple-translation-files)
 
 i18n
   .use(Backend)
@@ -16,20 +11,14 @@ i18n
   .use(initReactI18next)
   .init({
     fallbackLng: 'en',
-    partialBundledLanguages: true,
     supportedLngs: [...I18N_SUPPORTED_LANGUAGE_CODES],
     showSupportNotice: false,
     returnEmptyString: false,
     interpolation: {
       escapeValue: false // not needed for react as it escapes by default
     },
-    resources: {
-      en: {
-        translation: enTranslations
-      }
-    },
     backend: {
-      // Other languages will still fetch from this public URL
+      // Locale files live in public and are fetched at runtime.
       loadPath: '/locales/{{lng}}/{{ns}}.json'
     },
     react: {
