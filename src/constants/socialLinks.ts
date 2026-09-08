1f2aa7f78da9d13f2b046c1d6b301f5783ae3b3d
PvtVoid <jotrickr@gmail.com>
2026-09-08 16:03:19 +0200

    New sets coded cards link

--- src/constants/socialLinks.ts
+++ src/constants/socialLinks.ts
@@ -5,5 +5,7 @@ export const TALISHAR_BLUESKY_URL =
 export const TALISHAR_X_URL = 'https://twitter.com/talishar_online';
 export const TALISHAR_YOUTUBE_URL = 'https://www.youtube.com/@pvtvoid';
 export const TALISHAR_DISCORD_URL = 'https://discord.gg/JykuRkdd5S';
+export const TALISHAR_CODED_CARDS_URL =
+  'https://docs.google.com/spreadsheets/d/1ci_xWTPdC4WWqA9OEMRcxH_4ZcPRfQANl_UwnUyMCpk/edit?usp=sharing';
 export const JUDGE_HUB_DISCORD_URL =
   'https://discord.gg/flesh-and-blood-judge-hub-874145774135558164';
