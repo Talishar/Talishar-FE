e654c108e1c805e16de47f88193a504b8f4bbb9e
PvtVoid <jotrickr@gmail.com>
2026-09-24 09:11:38 +0200

    Drop the source image

--- src/routes/game/components/elements/playerInputPopUp/components/PromptCards.tsx
+++ src/routes/game/components/elements/playerInputPopUp/components/PromptCards.tsx
@@ -1,6 +1,5 @@
 import React, { useMemo } from 'react';
 import { useTranslation } from 'react-i18next';
-import classNames from 'classnames';
 import { Card } from 'features/Card';
 import { cardIDsInText } from 'utils/ParseEscapedString';
 import CardDisplay from '../../cardDisplay/CardDisplay';
@@ -22,28 +21,25 @@ export const PromptCards = ({
   deckTopIsOpponent
 }: PromptCardsProps) => {
   const { t } = useTranslation();
-  const sourceLabel = t('PLAYER_INPUT.SOURCE');
   const deckTopLabel = t(
     deckTopIsOpponent
       ? 'PLAYER_INPUT.TOP_OF_THEIR_DECK'
       : 'PLAYER_INPUT.TOP_OF_DECK'
   );
-  const { cards, source } = useMemo(() => {
-    const subjectCards: Card[] = cardIDsInText(title)
-      .filter((cardNumber) => cardNumber !== sourceCard)
-      .slice(0, MAX_PROMPT_CARDS)
-      .map((cardNumber) =>
-        cardNumber === deckTopCard
-          ? { cardNumber, label: deckTopLabel }
-          : { cardNumber }
-      );
-    const sourceCardProp: Card | undefined = sourceCard
-      ? { cardNumber: sourceCard, label: sourceLabel }
-      : undefined;
-    return { cards: subjectCards, source: sourceCardProp };
-  }, [title, sourceCard, deckTopCard, sourceLabel, deckTopLabel]);
+  const cards = useMemo(
+    (): Card[] =>
+      cardIDsInText(title)
+        .filter((cardNumber) => cardNumber !== sourceCard)
+        .slice(0, MAX_PROMPT_CARDS)
+        .map((cardNumber) =>
+          cardNumber === deckTopCard
+            ? { cardNumber, label: deckTopLabel }
+            : { cardNumber }
+        ),
+    [title, sourceCard, deckTopCard, deckTopLabel]
+  );
 
-  if (cards.length === 0 && !source) return null;
+  if (cards.length === 0) return null;
 
   return (
     <div className={styles.promptCards}>
@@ -52,15 +48,6 @@ export const PromptCards = ({
           <CardDisplay card={card} preventUseOnClick />
         </div>
       ))}
-      {source ? (
-        <div
-          className={classNames(styles.promptCard, {
-            [styles.promptSourceCard]: cards.length > 0
-          })}
-        >
-          <CardDisplay card={source} preventUseOnClick />
-        </div>
-      ) : null}
     </div>
   );
 };
